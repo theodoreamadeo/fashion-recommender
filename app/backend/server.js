f5aa3d33fd523be0cadfaae1f455be03467e6233
theodoreamadeo <THEO0013@e.ntu.edu.sg>
2025-02-04 14:26:34 +0800

    Feat: update the server connection

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,11 +1,12 @@
 import express from 'express';
+import { connectDB } from './config/db.js';
 
 const app = express();
 
-app.get("/", (req, res) => {
-    res.send("Server is ready");
-})
+app.get("/products", (req, res) => {});
 
 app.listen(5000, () => {
+    connectDB();
     console.log("Server started at http://localhost:5000");
 })
+
