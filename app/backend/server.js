4b1341b57b94152fec67b401df3bcf0ba1a3a99c
theodoreamadeo <THEO0013@e.ntu.edu.sg>
2025-02-04 15:58:56 +0800

    Feat: haven't start with web scrapping

--- app/backend/server.js
+++ app/backend/server.js
@@ -1,9 +1,29 @@
 import express from 'express';
 import { connectDB } from './config/db.js';
+import Product from './models/products.js';
 
 const app = express();
 
-app.get("/products", (req, res) => {});
+app.use(express.json()); //allows to accept JSON data in the req.body
+
+app.post("/api/products", async (req, res) => {
+    const product = req.body; //user will send this data
+
+    if (!product.image || !product.name || !product.price || !product.description || !product.size || !product.color) {
+        return res.status(400).json({success: false, message: "Please provide all fields"});
+    }
+
+    const newProduct = new Product (product);
+
+    try {
+        await newProduct.save();
+        res.status (201).json({success: true, data: newProduct});
+    } catch (error) {
+        console.error("Error in create product: ", error.message);
+        res.status (500).json({success: false, message: "Server Error"});
+    }
+
+});
 
 app.listen(5000, () => {
     connectDB();
